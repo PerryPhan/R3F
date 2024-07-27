cd5387c2608ca63087e457879d7f703504f46d83
PerryPhan <daiphan308@gmail.com>
2024-07-27 22:16:31 +0700

    Setup the hero gundam for the home

--- src/App.js
+++ src/App.js
@@ -2,19 +2,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 // Component
-import { Environment } from '@react-three/drei';
-import Box from './shape/Box';
+import { CameraControls, Environment, PerspectiveCamera, useHelper } from '@react-three/drei';
+import {GundamPhenex} from './components/GundamPhenex';
+import { useEffect, useRef } from 'react';
+import { folder, useControls } from 'leva';
+import { SpotLightHelper } from 'three';
 
 function App() {
   // Remember to press on screen to activate music. It is big to load
-
+  const {x, y, z, rx, ry, rz} = useControls({
+    position: folder({
+      x: 315,
+      y: -750,
+      z: 800
+    }),
+    rotation: folder({
+      rx: {
+        value: 0,
+        max: 1,
+        min: -1,
+        step: 0.1
+      },
+      ry: {
+        value: 0,
+        max: 1,
+        min: -1,
+        step: 0.1
+      },
+      rz: {
+        value: 0,
+        max: 1,
+        min: -1,
+        step: 0.1
+      }
+    })
+  })
+ 
   return (
     <>
-      <color attach='background' args={['#000000']} />
-      <ambientLight intensity={1} />
-      <spotLight position={[0, 25, 0]} angle={1.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001} />
+      <PerspectiveCamera position={[0, 50, 1000]} pov={3000} makeDefault />
+      <color attach='background' args={['#000']} />
+      {/* <spotLight position={[lx, ly, lz]} color={color} angle={3} penumbra={1} castShadow intensity={100} shadow-bias={-0.0001} /> */}
       <Environment preset='warehouse' />
-      <Box/>
+      <GundamPhenex position={[x, y, z]}  rotation={[rx, ry, rz ]}/>
     </>
   );
 }
